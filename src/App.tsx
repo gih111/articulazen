597b7542a62766e81ae8f22e64f3b835b56987fe
gih111 <dra.giovannaoliveiira@hotmail.com>
2025-07-24 22:47:41 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -180,7 +180,7 @@ function App() {
           <div className="max-w-6xl mx-auto px-4 relative z-10">
             <h1 className="text-2xl md:text-4xl lg:text-6xl font-black mb-4 md:mb-6 leading-tight">
              "Imagine voltar a fazer suas caminhadas matinais sem aquele desconforto
-              <span className="text-white block md:inline bg-orange-700 px-2 rounded"> persistente. </span>
+              <span className="text-white block md:inline bg-orange-700 px-2 rounded"> PERSISTENTE. </span>
               ou A fórmula natural que ajuda a manter o bem-estar das suas
               <span className="text-white block md:inline bg-orange-700 px-2 rounded"> ARTICULAÇÕES.</span>
             </h1>
