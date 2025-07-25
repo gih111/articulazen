5738f85e7ed942b5c1270f25f88ea1a6d5f290c5
gih111 <dra.giovannaoliveiira@hotmail.com>
2025-07-24 22:53:53 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -179,10 +179,10 @@ function App() {
           <div className="absolute inset-0 bg-black/10"></div>
           <div className="max-w-6xl mx-auto px-4 relative z-10">
             <h1 className="text-2xl md:text-4xl lg:text-6xl font-black mb-4 md:mb-6 leading-tight">
-             Imagine voltar a fazer suas caminhadas matinais sem aquele desconforto
-              <span className="text-white block md:inline bg-orange-700 px-2 rounded"> PERSISTENTE. </span>
-               A fórmula natural que ajuda a manter o bem-estar das suas
-              <span className="text-white block md:inline bg-orange-700 px-2 rounded"> ARTICULAÇÕES.</span>
+            MÉDICOS EM CHOQUE: Fórmula Natural "SECRETA" de Raiz Dourada pode ajudar a recuperar a mobilidade de forma natural e progressiva.
+              <span className="text-white block md:inline bg-orange-700 px-2 rounded"> forma natural e progressiva. </span>
+                Imagine voltar a fazer suas caminhadas matinais sem aquele desconforto
+              <span className="text-white block md:inline bg-orange-700 px-2 rounded"> PERSISTENTE.</span>
             </h1>
             <p className="text-lg md:text-xl lg:text-2xl mb-6 md:mb-8 opacity-90 max-w-4xl mx-auto">
               Junte-se aos +27.482 brasileiros que abandonaram os remédios caros e voltaram a viver sem o tormento da dor.
