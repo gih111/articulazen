a6a901fbccd7fdad211018911aa2fcabae42b12b
gih111 <dra.giovannaoliveiira@hotmail.com>
2025-07-24 22:45:28 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -179,10 +179,10 @@ function App() {
           <div className="absolute inset-0 bg-black/10"></div>
           <div className="max-w-6xl mx-auto px-4 relative z-10">
             <h1 className="text-2xl md:text-4xl lg:text-6xl font-black mb-4 md:mb-6 leading-tight">
-              MÉDICOS EM CHOQUE: Fórmula Natural "SECRETA" de Raiz Dourada 
-              <span className="text-white block md:inline bg-orange-700 px-2 rounded"> ATACA </span>
-              a Causa Raiz da Dor Articular e Devolve a Mobilidade em 
-              <span className="text-white block md:inline bg-orange-700 px-2 rounded"> 17 DIAS</span>
+             "Imagine voltar a fazer suas caminhadas matinais sem aquele desconforto ou "A fórmula natural que ajuda a manter o bem-estar das suas articulações. 
+              <span className="text-white block md:inline bg-orange-700 px-2 rounded"> persistente. </span>
+              ou "A fórmula natural que ajuda a manter o bem-estar das suas
+              <span className="text-white block md:inline bg-orange-700 px-2 rounded"> ARTICULAÇÕES.</span>
             </h1>
             <p className="text-lg md:text-xl lg:text-2xl mb-6 md:mb-8 opacity-90 max-w-4xl mx-auto">
               Junte-se aos +27.482 brasileiros que abandonaram os remédios caros e voltaram a viver sem o tormento da dor.
